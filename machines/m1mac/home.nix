70c018646104ab5c6d5bbdfae9cd2cb319fbfad4
lucidph3nx <ben@tinfoilforest.nz>
2024-09-16 20:14:05 +1200

    remove ssh stuff

--- machines/m1mac/home.nix
+++ machines/m1mac/home.nix
@@ -14,10 +14,6 @@
       workSSH.enable = true;
       kubeconfig.enable = true;
     };
-    ssh.client = {
-      enable = true;
-      workConfig.enable = true;
-    };
   };
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
