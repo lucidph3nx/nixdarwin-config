7d15384b8231cd8a53e6fb83e8d5415b77a01240
lucidph3nx <ben@tinfoilforest.nz>
2024-09-16 20:41:17 +1200

    remove ncmpcpp

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -12,7 +12,6 @@
     ./k9s.nix
     ./kubetools.nix
     ./lf.nix
-    ./ncmpcpp.nix
     ./tmux.nix
     ./tmuxSessioniser.nix
     ./zsh.nix
@@ -25,7 +24,6 @@
     k9s.enable = lib.mkDefault true;
     kubetools.enable = lib.mkDefault true;
     lf.enable = lib.mkDefault true;
-    ncmpcpp.enable = lib.mkDefault true;
     tmux.enable = lib.mkDefault true;
     tmuxSessioniser.enable = lib.mkDefault true;
     zsh.enable = lib.mkDefault true;
