4a0ff1c2a92ad1a4c04851d3b52103d748eaf0d2
lucidph3nx <ben@tinfoilforest.nz>
2024-09-18 10:34:57 +1200

    add nvim session script to aerospace

--- modules/home-manager/choose.nix
+++ modules/home-manager/choose.nix
@@ -7,14 +7,14 @@
 let
   terminal = "${pkgs.kitty}/bin/kitty";
 in {
-  imports = [../cli/tmuxSessioniser.nix]; #needed for one of the choose scripts
+  imports = [./cli/tmuxSessioniser.nix]; #needed for one of the choose scripts
   options = {
     homeManagerModules.choose.enable =
       lib.mkEnableOption "enables choose";
   };
-  config = lib.mkIf config.theme.choose.enable {
+  config = lib.mkIf config.homeManagerModules.choose.enable {
     home.packages = with pkgs; [
-      choose-ui
+      choose-gui
     ];
     homeManagerModules.tmuxSessioniser.enable = true;
     home.file.".local/scripts/application.nvim.sessionLauncher" = {
@@ -22,7 +22,7 @@ in {
       text = ''
         #!/bin/sh
         # Get the selection from tmux project getter
-        selection=$(~/.local/scripts/cli.tmux.projectGetter | choose )
+        selection=$(~/.local/scripts/cli.tmux.projectGetter | ${pkgs.choose-gui}/bin/choose )
         if [ -n "$selection" ]; then
           # Run the tmux sessioniser with the selected session
           xargs -I{} ${terminal} ~/.local/scripts/cli.tmux.projectSessioniser "{}" 2> /dev/null <<<"$selection"
