c3e50f1a60bdbc06fd1ad520b55facb55c8c9086
lucidph3nx <ben@tinfoilforest.nz>
2025-02-14 11:41:12 +1300

    tweaks to neovim github light theme

--- modules/home-manager/neovim/plugins/theme-github-light.nix
+++ modules/home-manager/neovim/plugins/theme-github-light.nix
@@ -13,6 +13,7 @@
       hash = "sha256-EreIuni6/XR0428rO4Lbi2usIreOyPWKm7kJJA2Nwqo=";
     };
   };
+  theme = config.theme;
 in {
   programs.neovim.plugins = lib.mkIf (config.theme.name == "github-light") [
     {
@@ -23,8 +24,62 @@ in {
         lua
         */
         ''
-          require('github-theme').setup()
-          vim.cmd('colorscheme github_light')
+          require("github-theme").setup({
+          	groups = {
+          		all = {
+          			ObsidianTodo = {
+          				fg = "${theme.blue}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          			ObsidianDone = {
+          				fg = "${theme.green}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          			ObsidianRightArrow = {
+          				fg = "${theme.blue}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          			ObsidianTilde = {
+          				fg = "${theme.orange}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          			ObsidianRefText = {
+          				fg = "${theme.blue}",
+          				bg = "none",
+          				style = "NONE", -- no underline for obsidian links
+          			},
+          			ObsidianExtLinkIcon = {
+          				fg = "${theme.blue}",
+          				bg = "none",
+          			},
+          			ObsidianTag = {
+          				fg = "${theme.blue}",
+          				bg = "none",
+          				style = "italic",
+          			},
+          			["@markup.heading.1.markdown"] = {
+          				fg = "${theme.blue}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          			["@markup.heading.2.markdown"] = {
+          				fg = "${theme.green}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          			["@markup.heading.3.markdown"] = {
+          				fg = "${theme.purple}",
+          				bg = "none",
+          				style = "bold",
+          			},
+          		},
+          	},
+          })
+          vim.cmd("colorscheme github_light")
         '';
     }
   ];
