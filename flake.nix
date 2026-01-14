4f5690d68e929f30bb7c8ee19c11d14d7a329e04
lucidph3nx <ben@tinfoilforest.nz>
2026-01-14 16:17:16 +1300

    Remove obsolete m1mac-test and x86mac machine configurations

--- flake.nix
+++ flake.nix
@@ -66,43 +66,6 @@
           home-manager.darwinModules.home-manager
         ];
       };
-      # temporary test machine
-      m1mac-test = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-          config.allowUnfree = true;
-          overlays = [ self.overlays.modifications ];
-        };
-        specialArgs = {inherit inputs outputs;};
-        modules = let
-          defaults = {pkgs, ...}: {
-            _module.args.pkgs-unstable = import inputs.nixpkgs-unstable {inherit (pkgs.stdenv.targetPlatform) system;};
-          };
-        in [
-          defaults
-          ./machines/m1mac-test/configuration.nix
-          home-manager.darwinModules.home-manager
-        ];
-      };
-      x86mac = darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        pkgs = import nixpkgs {
-          system = "x86_64-darwin";
-          config.allowUnfree = true;
-          overlays = [ self.overlays.modifications ];
-        };
-        specialArgs = {inherit inputs outputs;};
-        modules = let
-          defaults = {pkgs, ...}: {
-            _module.args.pkgs-unstable = import inputs.nixpkgs-unstable {inherit (pkgs.stdenv.targetPlatform) system;};
-          };
-        in [
-          defaults
-          ./machines/x86mac/configuration.nix
-          home-manager.darwinModules.home-manager
-        ];
-      };
     };
   };
 }
